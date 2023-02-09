48c478d5f2ca6c680bebd63b80732003b9746d46
Tsletten84 <88718470+Tsletten84@users.noreply.github.com>
2023-02-09 17:14:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
- let passwordOne = document.getElementById("password-one")
- let passwordTwo = document.getElementById("password-two")
-console.log(passwordOne)
 
+let passwordOne = document.getElementById("password-one")
+let passwordTwo = document.getElementById("password-two")
 
 
 document.getElementById("generator").addEventListener("click", function() {
-    getPasswordOne();
-    getPasswordTwo();
-  });
+    getPasswordOne()
+    getPasswordTwo()
+  })
 
-//generator.addEventListener("click", 
 function getPasswordOne(){
     let randomArray = []
 
@@ -20,8 +18,7 @@ function getPasswordOne(){
         let random = Math.floor(Math.random()*characters.length)
         randomArray.push(characters[random])          
     }
-passwordOne.textContent = randomArray.join("")
-console.log(randomArray)
+    passwordOne.textContent = randomArray.join("")
 }
 
 function getPasswordTwo(){
@@ -33,8 +30,4 @@ function getPasswordTwo(){
         randomArray.push(characters[random])
     }
 passwordTwo.textContent = randomArray.join("")
-
-console.log(randomArray)
 }
-
-
